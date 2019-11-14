c6b4b96e80db823c177dc6ad171129b0b9f1615d
Benjamin Small <ben20262@aim.com>
2019-11-14 22:03:35 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,9 +6,11 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  word = []
   src.each do |array|
-    "I love #{array[0]} and #{array[1]} on my pizza"
+    word << "I love #{array[0]} and #{array[1]} on my pizza"
   end
+  word
 end
 
 def find_greater_pair(src)
